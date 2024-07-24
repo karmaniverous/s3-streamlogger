7755c7dd87632e955dec3b76816483a7ff967357
Jason Williscroft <me@karmanivero.us>
2024-07-24 22:19:58 +0800

    wip unit tests

---
+++ test/setup.ts
@@ -0,0 +1,15 @@
+/* eslint-env mocha */
+
+// import { $ } from 'execa';
+
+before(async function () {
+  // acquire a credential
+  //   const { stdout } =
+  //     await $`aws configure export-credentials --profile ${profile}`;
+  //   const {
+  //     AccessKeyId: AWS_ACCESS_KEY_ID,
+  //     SecretAccessKey: AWS_SECRET_ACCESS_KEY,
+  //     SessionToken: AWS_SESSION_TOKEN, // comes from aws sso login --profile
+  //   } = JSON.parse(stdout.toString()) as Record<string, string>;
+  // set environment variables
+});
